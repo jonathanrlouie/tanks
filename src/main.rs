e83d0287aa1c40c80411192b0f64e2fbec7c62bd
Jonathan Louie <jonathanl@bitquilltech.com>
2021-05-15 19:31:57 -0700

    Add state swapping on win or lose

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::{
     prelude::*,
+    render::camera::Camera,
     sprite::collide_aabb::{collide, Collision},
 };
 
@@ -7,21 +8,40 @@ fn main() {
     App::build()
         .insert_resource(CursorPosition { pos: Vec2::ZERO })
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup.system())
-        .add_system(player_movement_system.system())
-        .add_system(player_shoot_system.system())
-        .add_system(bullet_movement_system.system())
-        .add_system(cursor_position_system.system())
-        .add_system(bullet_cleanup_system.system())
-        .add_system(bullet_collision_system.system())
-        .add_system(brown_tank_shoot_system.system())
-        .add_system(win_or_lose_system.system())
+        .add_state(GameState::Playing)
+        .add_startup_system(setup_camera.system())
+        .add_system_set(SystemSet::on_enter(GameState::Playing).with_system(setup.system()))
+        .add_system_set(
+            SystemSet::on_update(GameState::Playing)
+                .with_system(player_movement_system.system())
+                .with_system(player_shoot_system.system())
+                .with_system(bullet_movement_system.system())
+                .with_system(cursor_position_system.system())
+                .with_system(bullet_cleanup_system.system())
+                .with_system(bullet_collision_system.system())
+                .with_system(brown_tank_shoot_system.system())
+                .with_system(playing_system.system()),
+        )
+        .add_system_set(
+            SystemSet::on_enter(GameState::Lose).with_system(lose_setup_system.system()),
+        )
+        .add_system_set(SystemSet::on_update(GameState::Lose).with_system(lose_system.system()))
+        .add_system_set(SystemSet::on_exit(GameState::Lose).with_system(teardown_system.system()))
         .run()
 }
 
-struct CursorPosition {
-    pos: Vec2
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+enum GameState {
+    Win,
+    Lose,
+    Playing,
 }
+
+struct CursorPosition {
+    pos: Vec2,
+}
+
+struct LoseTimer(Timer);
 
 enum Collider {
     Wall,
@@ -38,38 +58,32 @@ struct Bullet {
     velocity: Vec3,
 }
 
-struct RicochetLimit {
-    limit: u32,
-}
+struct RicochetLimit(u32);
 
-struct RicochetCount {
-    count: u32,
-}
+struct RicochetCount(u32);
 
-struct BulletOwner {
-    owner: Entity
-}
+struct BulletOwner(Entity);
 
-struct BulletLimit {
-    limit: u8,
-}
+struct BulletLimit(u8);
 
 struct Enemy;
 
 struct BrownTank;
 
+fn setup_camera(mut commands: Commands) {
+    // camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
 fn setup(
-    mut commands: Commands,
+    commands: Commands,
     asset_server: Res<AssetServer>,
     materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    // camera
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-
     let mut creator = Creator {
         commands,
         asset_server,
-        materials
+        materials,
     };
 
     // player
@@ -101,7 +115,7 @@ impl<'a> Creator<'a> {
                 ..Default::default()
             })
             .insert(Player { speed: 100.0 })
-            .insert(BulletLimit { limit: 5 })
+            .insert(BulletLimit(5))
             .insert(Collider::Player);
     }
 
@@ -124,20 +138,22 @@ impl<'a> Creator<'a> {
                 transform: Transform::from_xyz(x, y, 0.0),
                 ..Default::default()
             })
-            .insert(BulletLimit { limit: 1 })
+            .insert(BulletLimit(1))
             .insert(BrownTank)
             .insert(Enemy)
             .insert(Collider::Enemy);
     }
 }
 
+#[allow(clippy::type_complexity)]
 fn player_movement_system(
     time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
     mut queries: QuerySet<(
         Query<(&Transform, &Sprite), With<Player>>,
         Query<(&Collider, &Transform, &Sprite)>,
-        Query<(&Player, &mut Transform)>)>,
+        Query<(&Player, &mut Transform)>,
+    )>,
 ) {
     let mut collisions: Vec<Collision> = vec![];
     if let Ok((player_transform, player_sprite)) = queries.q0().single() {
@@ -146,13 +162,13 @@ fn player_movement_system(
                 player_transform.translation,
                 player_sprite.size,
                 transform.translation,
-                sprite.size
+                sprite.size,
             );
 
             if let Some(collision) = collision {
                 match *collider {
                     Collider::Wall | Collider::Enemy => collisions.push(collision),
-                    _ => ()
+                    _ => (),
                 }
             }
         }
@@ -176,7 +192,7 @@ fn player_movement_system(
             direction -= Vec2::Y;
         }
 
-        let normalized_direction = direction.try_normalize().unwrap_or_else(|| Vec2::ZERO);
+        let normalized_direction = direction.try_normalize().unwrap_or(Vec2::ZERO);
         let translation = &mut player_transform.translation;
 
         let mut stop_x = false;
@@ -201,10 +217,7 @@ fn player_movement_system(
     }
 }
 
-fn cursor_position_system(
-    windows: Res<Windows>,
-    mut cursor_position: ResMut<CursorPosition>,
-) {
+fn cursor_position_system(windows: Res<Windows>, mut cursor_position: ResMut<CursorPosition>) {
     if let Some(cursor_pos) = calculate_cursor_position(windows) {
         cursor_position.pos = cursor_pos;
     }
@@ -213,7 +226,10 @@ fn cursor_position_system(
 fn calculate_cursor_position(windows: Res<Windows>) -> Option<Vec2> {
     let window = windows.get_primary()?;
     let cursor_position = window.cursor_position()?;
-    Some(Vec2::new(cursor_position.x - window.width() / 2.0, cursor_position.y - window.height() / 2.0))
+    Some(Vec2::new(
+        cursor_position.x - window.width() / 2.0,
+        cursor_position.y - window.height() / 2.0,
+    ))
 }
 
 fn bullet_cleanup_system(
@@ -223,10 +239,10 @@ fn bullet_cleanup_system(
 ) {
     if let Some(window) = windows.get_primary() {
         for (entity, transform) in query.iter() {
-            if transform.translation.x > window.width() / 2.0 ||
-                transform.translation.x < 0.0 - window.width() / 2.0 ||
-                transform.translation.y > window.height() / 2.0 ||
-                transform.translation.y < 0.0 - window.height() / 2.0
+            if transform.translation.x > window.width() / 2.0
+                || transform.translation.x < 0.0 - window.width() / 2.0
+                || transform.translation.y > window.height() / 2.0
+                || transform.translation.y < 0.0 - window.height() / 2.0
             {
                 commands.entity(entity).despawn();
             }
@@ -244,22 +260,26 @@ fn player_shoot_system(
     cursor_position: Res<CursorPosition>,
 ) {
     if let Ok((player_entity, bullet_limit, player_transform)) = query.single() {
-        if mouse_input.just_pressed(MouseButton::Left) &&
-            bullet_query
+        if mouse_input.just_pressed(MouseButton::Left)
+            && bullet_query
                 .iter()
-                .filter(|owner| owner.owner == player_entity)
-                .count() < bullet_limit.limit.into() 
+                .filter(|owner| owner.0 == player_entity)
+                .count()
+                < bullet_limit.0.into()
         {
             if let Some(bullet_direction) = Vec3::new(
                 cursor_position.pos.x - player_transform.translation.x,
                 cursor_position.pos.y - player_transform.translation.y,
-                0.0
-            ).try_normalize() {
+                0.0,
+            )
+            .try_normalize()
+            {
                 let texture_handle = asset_server.load("bullet.png");
                 let bullet_transform = Transform::from_xyz(
                     player_transform.translation.x,
                     player_transform.translation.y,
-                    0.0);
+                    0.0,
+                );
                 let sprite_bundle = SpriteBundle {
                     material: materials.add(texture_handle.into()),
                     transform: bullet_transform,
@@ -269,20 +289,19 @@ fn player_shoot_system(
                 // bullet
                 commands
                     .spawn_bundle(sprite_bundle)
-                    .insert(Bullet { velocity: 150.0 * bullet_direction })
-                    .insert(BulletOwner { owner: player_entity })
-                    .insert(RicochetLimit { limit: 1 })
-                    .insert(RicochetCount { count: 0 })
+                    .insert(Bullet {
+                        velocity: 150.0 * bullet_direction,
+                    })
+                    .insert(BulletOwner(player_entity))
+                    .insert(RicochetLimit(1))
+                    .insert(RicochetCount(0))
                     .insert(Collider::Bullet);
             }
         }
     }
 }
 
-fn bullet_movement_system(
-    time: Res<Time>,
-    mut query: Query<(&Bullet, &mut Transform)>,
-) {
+fn bullet_movement_system(time: Res<Time>, mut query: Query<(&Bullet, &mut Transform)>) {
     for (bullet, mut transform) in query.iter_mut() {
         transform.translation += time.delta_seconds() * bullet.velocity;
     }
@@ -290,28 +309,45 @@ fn bullet_movement_system(
 
 fn bullet_collision_system(
     mut commands: Commands,
-    mut bullet_query: Query<(Entity, &mut Bullet, &BulletOwner, &RicochetLimit, &mut RicochetCount, &Transform, &Sprite)>,
+    mut bullet_query: Query<(
+        Entity,
+        &mut Bullet,
+        &BulletOwner,
+        &RicochetLimit,
+        &mut RicochetCount,
+        &Transform,
+        &Sprite,
+    )>,
     collider_query: Query<(Entity, &Collider, &Transform, &Sprite)>,
 ) {
-    for (bullet_entity, mut bullet, bullet_owner, ricochet_limit, mut ricochet_count, bullet_transform, bullet_sprite) in bullet_query.iter_mut() {
+    for (
+        bullet_entity,
+        mut bullet,
+        bullet_owner,
+        ricochet_limit,
+        mut ricochet_count,
+        bullet_transform,
+        bullet_sprite,
+    ) in bullet_query.iter_mut()
+    {
         let velocity = &mut bullet.velocity;
         for (collider_entity, collider, transform, sprite) in collider_query.iter() {
             let collision = collide(
                 bullet_transform.translation,
                 bullet_sprite.size,
                 transform.translation,
-                sprite.size
+                sprite.size,
             );
 
             if let Some(collision) = collision {
                 match *collider {
                     Collider::Bullet => commands.entity(collider_entity).despawn(),
                     Collider::Enemy | Collider::Player => {
-                        if !(bullet_owner.owner == collider_entity && ricochet_count.count < 1) {
+                        if !(bullet_owner.0 == collider_entity && ricochet_count.0 < 1) {
                             commands.entity(bullet_entity).despawn();
                             commands.entity(collider_entity).despawn()
                         }
-                    },
+                    }
                     Collider::Wall => {
                         let mut reflect_x = false;
                         let mut reflect_y = false;
@@ -323,7 +359,7 @@ fn bullet_collision_system(
                             Collision::Bottom => reflect_y = velocity.y > 0.0,
                         }
 
-                        if ricochet_count.count >= ricochet_limit.limit && (reflect_x || reflect_y) {
+                        if ricochet_count.0 >= ricochet_limit.0 && (reflect_x || reflect_y) {
                             commands.entity(bullet_entity).despawn();
                         } else {
                             if reflect_x {
@@ -335,7 +371,7 @@ fn bullet_collision_system(
                             }
 
                             if reflect_x || reflect_y {
-                                ricochet_count.count += 1;
+                                ricochet_count.0 += 1;
                             }
                         }
                     }
@@ -355,20 +391,25 @@ fn brown_tank_shoot_system(
 ) {
     for (tank_entity, bullet_limit, tank_transform) in brown_tank_query.iter() {
         if let Ok(player_transform) = player_query.single() {
-            if bullet_query.iter()
-                .filter(|owner| owner.owner == tank_entity)
-                .count() < bullet_limit.limit.into()
+            if bullet_query
+                .iter()
+                .filter(|owner| owner.0 == tank_entity)
+                .count()
+                < bullet_limit.0.into()
             {
                 if let Some(bullet_direction) = Vec3::new(
                     player_transform.translation.x - tank_transform.translation.x,
                     player_transform.translation.y - tank_transform.translation.y,
-                    0.0
-                ).try_normalize() {
+                    0.0,
+                )
+                .try_normalize()
+                {
                     let texture_handle = asset_server.load("bullet.png");
                     let bullet_transform = Transform::from_xyz(
                         tank_transform.translation.x,
                         tank_transform.translation.y,
-                        0.0);
+                        0.0,
+                    );
                     let sprite_bundle = SpriteBundle {
                         material: materials.add(texture_handle.into()),
                         transform: bullet_transform,
@@ -377,10 +418,12 @@ fn brown_tank_shoot_system(
 
                     commands
                         .spawn_bundle(sprite_bundle)
-                        .insert(Bullet { velocity: 150.0 * bullet_direction })
-                        .insert(BulletOwner { owner: tank_entity })
-                        .insert(RicochetLimit { limit: 1 })
-                        .insert(RicochetCount { count: 0 })
+                        .insert(Bullet {
+                            velocity: 150.0 * bullet_direction,
+                        })
+                        .insert(BulletOwner(tank_entity))
+                        .insert(RicochetLimit(1))
+                        .insert(RicochetCount(0))
                         .insert(Collider::Bullet);
                 }
             }
@@ -388,16 +431,44 @@ fn brown_tank_shoot_system(
     }
 }
 
-fn win_or_lose_system(
+fn playing_system(
     player_query: Query<&Player>,
     enemy_query: Query<&Enemy>,
+    mut game_state: ResMut<State<GameState>>,
 ) {
     if enemy_query.iter().count() == 0 {
-        println!("You win!")
+        game_state
+            .set(GameState::Win)
+            .expect("Error: Failed to push Win state");
+    } else if player_query.single().is_err() {
+        game_state
+            .set(GameState::Lose)
+            .expect("Error: Failed to push Lose state")
     }
+}
 
-    match player_query.single() {
-        Err(_) => println!("You lose!"),
-        _ => (),
+fn lose_setup_system(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(LoseTimer(Timer::from_seconds(4.0, false)));
+}
+
+fn teardown_system(mut commands: Commands, entities: Query<Entity, Without<Camera>>) {
+    for entity in entities.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn lose_system(
+    time: Res<Time>,
+    mut query: Query<&mut LoseTimer>,
+    mut game_state: ResMut<State<GameState>>,
+) {
+    if let Ok(mut timer) = query.single_mut() {
+        if timer.0.tick(time.delta()).just_finished() {
+            game_state
+                .set(GameState::Playing)
+                .expect("Error: Failed to set Playing state");
+        }
     }
 }
